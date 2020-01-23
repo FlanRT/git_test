1d433b546dc4f67f099748ea08061099855bed7e
FlanRT <abakajanee@outlook.com>
2020-01-22 21:31:10 -0600

    added the code for hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <climits>
+
+int main() {
+    std::vector<int> vNumbers;
+
+    srand(static_cast<unsigned>(time(0)));
+
+    for (int i = 0; i < 100; i++)
+        vNumbers.push_back(rand() % 100);
+
+    for (int i = 0; i < vNumbers.size(); i++)
+        std::cout << vNumbers[i] << ',';
+    std::cout << '\n';
+
+    int nSmallest = INT_MAX;
+    for (int i = 0; i < vNumbers.size(); i++)
+        if (vNumbers[i] < nSmallest)
+            nSmallest = vNumbers[i];
+    
+    std::cout << "The smallest number is " << nSmallest << '\n';
+}
